cbb7554270778f7a7b61ee507e5afc343c284368
Sheng Xu <49282686+little88125@users.noreply.github.com>
2019-07-12 09:11:58 -0700

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -17,3 +17,7 @@ shxu_finalData <- cbind(shxu_SetTime, shxu_finalData)
 
 ## Generating Plot 1##
 hist(shxu_finalData$Global_active_power, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+
+## Save file and close device##
+dev.copy(png,"plot1.png", width=480, height=480)
+dev.off()
