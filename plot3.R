0b12203cec7c9b0591ca4fbcb0a4e08c725461e1
Sheng Xu <49282686+little88125@users.noreply.github.com>
2019-07-12 09:18:08 -0700

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,3 +1,20 @@
+shxu_URL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+shxu_householdFile <- "./household_power_consumption.txt"
+shxu_downloadFile <- "./household_power_consumption.zip"
+
+##Download file##
+if (!file.exists(shxu_householdFile)){
+    download.file(shxu_downloadURL,shxu_downloadFile)
+    unzip(shxu_downloadFile,overwrite = T, exdir = ".")
+}
+
+shxu_plotData <- read.table(shxu_householdFile, header = T, sep = ";", na.string="?")
+
+##Set up time variable##
+shxu_finalData <- shxu_plotData[shxu_plotData$Date %in% c("1/2/2007","2/2/2007"),]
+shxu_SetTime <-strptime(paste(shxu_finalData$Date, shxu_finalData$Time, sep=" "),"%d/%m/%Y %H:%M:%S")
+shxu_finalData <- cbind(shxu_SetTime, shxu_finalData)
+
 ## Generating Plot 3##
 columnlines <- c("black", "red", "blue")
 labels <- c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3")
