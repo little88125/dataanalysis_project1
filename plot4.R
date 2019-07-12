402bc79aa6d3b65cbe901d332d55e940cba9b82f
Sheng Xu <49282686+little88125@users.noreply.github.com>
2019-07-12 09:31:43 -0700

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -25,7 +25,7 @@ plot(shxu_finalData$shxu_SetTime, shxu_finalData$Sub_metering_1, type="l", xlab=
 lines(shxu_finalData$shxu_SetTime, shxu_finalData$Sub_metering_2, type="l", col="red")
 lines(shxu_finalData$shxu_SetTime, shxu_finalData$Sub_metering_3, type="l", col="blue")
 legend("topright", bty="n", legend=labels, lty=1, col=columnlines)
-plot(shxu_finalData$shxu_SetTime, shxu_finalData$Global_reactive_power, type="l", col="blue", xlab="datetime", ylab="Global_reactive_power")
+plot(shxu_finalData$shxu_SetTime, shxu_finalData$Global_reactive_power, type="l", col="black", xlab="datetime", ylab="Global_reactive_power")
 
 ## Save as png ##
 dev.copy(png,"plot4.png", width=480, height=480)
